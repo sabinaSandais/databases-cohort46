70933583169d80c6e475c5f7c6efd1b1f56a467d
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-02-27 20:41:37 +0100

    Wk-2 Assignment-done

---
+++ Week2/Assignment/aggregate_functions.js
@@ -0,0 +1,73 @@
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "hw_week2",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database!");
+});
+
+// Query 1: All research papers and the number of authors that wrote that paper.
+connection.query(
+  `
+    SELECT rp.paper_title, COUNT(ap.author_id) AS num_authors
+    FROM research_Papers AS rp
+    LEFT JOIN author_Paper AS ap ON rp.paper_id = ap.paper_id
+    GROUP BY rp.paper_id`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Research Papers and Number of Authors logged!");
+    console.table(results);
+  }
+);
+
+ // Query 3: Average of the h-index of all authors per university.
+connection.query(
+  `
+    SELECT university, AVG(h_index) AS avg_h_index
+    FROM authors
+    GROUP BY university`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Average H-index per University logged!");
+    console.table(results);
+  }
+);
+
+ // Query 4: Sum of the research papers of the authors per university.
+connection.query(
+  `
+    SELECT a.university, SUM(rp_count) AS total_papers
+    FROM authors AS a
+    LEFT JOIN (
+      SELECT ap.author_id, COUNT(ap.paper_id) AS rp_count
+      FROM author_Paper AS ap
+      GROUP BY ap.author_id
+    ) AS author_paper_counts ON a.author_id = author_paper_counts.author_id
+    GROUP BY a.university`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Total Research Papers per University logged!");
+    console.table(results);
+  }
+);
+
+// Query 5: Minimum and maximum of the h-index of all authors per university.
+connection.query(
+  `
+    SELECT university, MIN(h_index) AS min_h_index, MAX(h_index) AS max_h_index
+    FROM authors
+    GROUP BY university`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Min and Max H-index per University logged!");
+    console.table(results);
+  }
+);
+
+connection.end();
